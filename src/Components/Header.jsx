012e91edaaabc858dd001a16ec2ef43f432136f3
Earnieball <svensson.kim88@gmail.com>
2022-10-21 16:17:12 +0200

    header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,81 +3,112 @@ import React from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
-import headerPicture from '../Assets/nature.jpg'
-import profilePicture from '../Assets/profile.jpg'
+import HeaderPicture from '../Assets/abstract.jpg'
+import ProfilePicture from '../Assets/profile.jpg'
 
 const HeaderBackground = styled.header`
-background-image: url(${headerPicture});
-display: flex;
-flex-direction: column;
-position: relative;
-background-position: stretch;
-width:100%;
-height: 500px;
-`
-const profilePicure = styled.img`
-image: url(${profilePicture});
-position: absolute;
-width: 100px;
-height: 100px;
+ 
+background-image: url(${HeaderPicture});
+  background-size: cover;
+  position: relative;
+  height: 500px;
+  resize: both;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 40px;
+    margin: 0px 0px 0 30px;
+    color: white;
+  }
 `
 
 const IconWrapper = styled.div`
-display: flex;
-justify-content: end;
-margin: 20px;
+position: relative;
+display:flex;
 place-items: flex-end;
-
-img {
-  width:50px;
-}
+justify-content: end;
+right: 390px;
+top: 20px;
 
 a {
-  margin: 10px;
-  transition: all 0.2s ease-in-out;
-
-  &hover {
-    transform: scale (1.1);
+     margin-right: 20px;
+    transition: all 0.2s ease-in-out;
+    &:hover {
+      transform: scale(1.1);
+      
+    }
   }
+`
+
+const ProfileImage = styled.img`
+    position:absolute;
+    width: 250px;
+    margin: 50px;
+    right: 300px;
+    top: 260px;
+    border-radius: 50%;
+    border: 2px solid #ffffff;
+    `
+
+const Presentation = styled.section`
+    margin-top: 20px;
+    width: 375px;
+    margin-left: 20%;
+    height: 300px;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    text-transform: uppercase;
+    
+    
+p {
+  font-size: 120%;
+  margin-left: 30px;
+}
+span {
+font-weight: bold;
 }
 
 h1 {
-  font-size: 200px;
-  margin: 0px 0px 30px;
+ text-transform: lowercase;
+ font-size: 42px;
+ line-height: 49px;
+ 
 }
+    `
 
-`
-
-export const Header = () => {
-  return (
+export const Header = () => (
+  <>
     <HeaderBackground>
       <IconWrapper>
         <a
           href="https://github.com/Earnieball"
           target="_blank"
           rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithub} size="3x" color="#212427" />
+          <FontAwesomeIcon icon={faGithub} size="3x" color="#000000" />
         </a>
         <a
           href="https://www.linkedin.com/in/kim-svensson-149ba017a/"
           target="_blank"
           rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faLinkedin} size="3x" color="#212427" />
+          <FontAwesomeIcon icon={faLinkedin} size="3x" color="#000000" />
         </a>
         <a
           href="https://stackoverflow.com/c/technigo/users/401"
           target="_blank"
           rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faStackOverflow} size="3x" color="#212427" />
+          <FontAwesomeIcon icon={faStackOverflow} size="3x" color="#000000" />
         </a>
       </IconWrapper>
 
-      <section>
+      <Presentation>
+        <p>Portfolio: <span>Kim Svensson</span></p>
         <h1>Frontend</h1>
         <h1>developer</h1>
-      </section>
+      </Presentation>
 
     </HeaderBackground>
-
-  )
-}
+    <ProfileImage src={ProfilePicture} />
+  </>
+)
